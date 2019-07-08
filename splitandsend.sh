928fcef2139d18c42291da3840cf9f97386ee9b4
Francis GILLES <fgilles@datasolution.fr>
2019-07-08 16:58:01 +0200

    rename and added usage

--- splitandsend.sh
+++ splitandsend.sh
@@ -24,17 +24,64 @@ BOLD="\033[1m"
 UNDERSCORE="\033[4m"
 REVERSE="\033[7m"
 
+###
+# USAGE & OPTIONS
+###
+OPT_VERBOSE="--verbose"
+OPT_VERSION="--version"
+
+
+# usage
+function usage() {
+    cat <<EOF
+    Envoie un fichier ou un dossier via Firefox Send
+    Usage
+    =====
+        $splitandsend source [$OPT_CONF=""] [email_destination]
+    Options
+    =======
+        $OPT_VERBOSE
+            Affiche les commandes exécutées et le détail de leur sortie
+        $OPT_VERSION
+            Affiche la version courante de $splitandsend
+        $OPT_SQL | $OPT_FILES
+            Ne synchroniser que la base de données, ou que les fichiers.
+            Par défaut : synchroniser la base de données et les fichiers.
+EOF
+}
+
+# parse parameters
+while [ $# -ne 0 ]
+do
+    case "$1" in
+        $OPT_VERBOSE)
+            verbose="1"
+            ;;
+        $OPT_VERSION)
+            echo "$splitandsend version $sitesync_version"
+            exit
+            ;;
+        -h|--help|*)
+            usage
+            exit
+            ;;
+    esac
+    shift
+done
+exit
+
 CURRENT_FOLDER=`pwd`
 FULLFILE="your_file"
 FILENAME="${FULLFILE##*/}"
 EXTENSION="${FILENAME##*.}"
 FILENAME="${FILENAME%.*}"
-
 # @TODO : get file by parameter and destination email insteaqd inserting them by hand :)
 
 ########
 # @TODO VERIFY FILE EXISTS
 ########
+
+
 echo -e "${YELLOW}Vérification du fichier${NORMAL}"
 
 mkdir $FILENAME
